ac4cef23214ea108d77d29b182bcbc7e2af235da
EINNNA <annie_t_@hotmail.com>
2019-10-19 21:01:22 +1100

    changed some of the js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,56 +14,46 @@ var connection = mysql.createConnection({
 });
 
 //VARIABLES
-
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err.message;
-    
+    table();
 });
 
+function table() {
     connection.query(
         "SELECT * FROM products", (err, res) => {
-            if (err) throw err.message; 
-        console.table(res);
-    });
-        function buyingItem() {
-            inquirer.prompt([
-                { 
-                    type: 'list',
-                    name: 'buyingItem',
-                    message: 'Which item do you want to buy?',
-                    choices: function () {
-                        var choices = [];
-                        res.forEach(function (product) {
-                            choices.push(chalk.green("SKU") + product.item_id + " || " + chalk.green("Price: $") + product.price + " || " + chalk.green("Product: ") + product.product_name);
-                        });
-                        return choices;
-                    }.then(answers => {
-                        inquirer.prompt([
-                            { 
-                                type: 'input',
-                                name: 'buyingItem',
-                                message: 'Which item do you want to buy?(Please use SKU)'
-                            }, 
-                            {
-                                type: 'input',
-                                name: 'itemAmount',
-                                message: 'How many do you want to buy?'
-                            }
-                    ]).then(answers => {
-                        var amount = parseInt(answers.itemAmount);
-                        var itemBought = answers.buyingItem;
+            if (err) throw err.message;
+            console.table(res);
+            buyingItem();
+        });
+}
 
-                        if (itemBought == product.product_name && amount > 0){
-                            connection.query(
-                                "UPDATE products SET stock_quantity WHERE 'item_id' =" + itemBought + ";"
-                                )
-                            } else {
-                                console.log("Out of stock, we are not able to supply this, please pick another item")
-                                buyingItem();
-                            }
+function buyingItem() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'buyingItem',
+            message: 'Which item do you want to buy?',
+            choices: function () {
+                var choices = [];
+                res.forEach(function (product) {
+                    choices.push(chalk.green("SKU") + product.item_id + " || " + chalk.green("Price: $") + product.price + " || " + chalk.green("Product: ") + product.product_name);
+                });
+                return choices;
+            }
+        }]).then(answers => {
+            var amount = parseInt(answers.itemAmount);
+            var itemBought = answers.buyingItem;
 
-                            }
-                        buyingAmount();
-                        })
-                ]);
-    };
+            if (itemBought == product.product_name && amount > 0) {
+                connection.query(
+                    "UPDATE products SET stock_quantity WHERE 'item_id' =" + itemBought + ";"
+                )
+            } else {
+                console.log("Out of stock, we are not able to supply this, please pick another item")
+                buyingItem();
+            }
+        });
+};
+
+buyingItem();
